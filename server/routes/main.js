390f80bcbd2d7603767e548800e0c7207373e13e
victor <victormutua71@gmail.com>
2024-01-17 08:59:05 -0800

    admin sections

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,8 +5,12 @@ var router = express.Router();
  * Load main page
  */
 router.get('/dashboard', (req, res)=>{
+
+    const successMessage = req.flash('success');
+
     res.render('admin/dash', {
-        title: "Main Dashboard page"
+        title: "Main Dashboard page",
+        successMessage
     })
 })
 
@@ -14,7 +18,13 @@ router.get('/dashboard', (req, res)=>{
  * Load Skills Page
  */
 router.get('/skills', (req, res)=>{
-    res.render('admin/skills')
+
+    const successMessage = req.flash('success');
+
+    res.render('admin/skills', {
+        title: "Admin | Skills Page",
+        successMessage
+    })
 })
 
 router.post('/add_skill', (req, res)=>{
@@ -34,7 +44,13 @@ router.post('/delete_skill', (req, res)=>{
  * Load Projects Page
  */
 router.get('/projects', (req, res)=>{
-    res.render('admin/projects')
+
+    const successMessage = req.flash('success');
+
+    res.render('admin/projects', {
+        title: "Admin | Projects Page",
+        successMessage
+    })
 })
 
 router.post('/add_project', (req, res)=>{
@@ -54,7 +70,13 @@ router.post('/delete_project', (req, res)=>{
  * Load Personal Information Page
  */
 router.get('/user_info', (req, res)=>{
-    res.render('admin/userInfo')
+
+    const successMessage = req.flash('success');
+
+    res.render('admin/userInfo', {
+        title: "Admin | User Information",
+        successMessage
+    })
 })
 
 router.post('/add_user_info', (req, res)=>{
@@ -73,7 +95,13 @@ router.post('/delete_user_info', (req, res)=>{
  * Load Testimonial Page
  */
 router.get('/testimonials', (req, res)=>{
-    res.render('admin/testimonials')
+
+    const successMessage = req.flash('success');
+
+    res.render('admin/testimonials', {
+        title: "Admin | Testimonials",
+        successMessage
+    })
 })
 
 router.post('/add_testimonial', (req, res)=>{
