307aa383f1f0e1815b5e2223f698d7f5ce8050a6
victor <victormutua71@gmail.com>
2024-01-17 02:36:38 -0800

    admin section

---
+++ server/models/user.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+
+
+//User info schema
+var userSchema = mongoose.Schema({
+
+    username:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [{
+            skillname: String,
+            percentage: Number
+        }]
+    },
+    projects: {
+        type: [{
+            projectname: String,
+            imageurl: String,
+            description: String,
+            projecturl: String
+        }]
+    },
+    workExperience: {
+        type: [{
+            workTitle: String,
+            years: {
+                from: String,
+                to: String
+            },
+            description: String
+        }]
+    }
+})
+
+
+
+var User = module.exports = mongoose.model('User', userSchema);
